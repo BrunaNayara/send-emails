383409f14f481e3ebb18cc751fd0f2a6ad28bca0
Bruna Moreira <brunanayaramlima@gmail.com>
2019-08-31 10:12:53 -0300

    Send same email to all receivers

--- send_mail2.py
+++ send_mail2.py
@@ -8,6 +8,7 @@ from email.mime.text import MIMEText
 
 from bs4 import BeautifulSoup
 
+##### Beautiful Soup methods
 # Get email template
 def get_template(template):
     with open(template) as fp:
@@ -22,7 +23,6 @@ def custom_greeting(name, template):
 
     return template
 
-
 # Change made by
 def change_sender_name(sender, template):
     greetings = template.select_one("#sender")
@@ -31,6 +31,7 @@ def change_sender_name(sender, template):
     return template
 
 
+##### Email and mime methods
 # create mail message
 def create_mail(subject, body):
     msg = MIMEText(body, 'html')
@@ -39,14 +40,16 @@ def create_mail(subject, body):
 
     return msg
 
-def change_receiver(msg, receiver):
+def change_receiver(receiver, msg):
     #receiver{ name: "str", email: "email@mail.com"}
+    del msg['To']
     msg['To'] = receiver['email']
 
     return msg
 
+##### SMTP methods
+# send email
 def send(msg):
-
     # get sender mail from file
     with open("mail.txt") as f:
         me = f.readline()
@@ -60,7 +63,47 @@ def send(msg):
     server.send_message(msg)
     server.quit()
 
-def send_my_email(template_file, sender_name, receiver_name, receiver_email):
+
+##### Send functions
+def send_to_list(mail_list, msg):
+    for receiver in mail_list:
+        send_to_one(receiver, msg)
+
+def send_to_one(receiver, msg):
+    msg = change_receiver(receiver, msg)
+    send(msg)
+
+def prepare_message(template_file, sender):
+    soup = get_template(template_file)
+    soup = change_sender_name(sender, soup)
+    mail = create_mail("subject", soup)
+
+    return mail
+
+
+def main():
+    tf = "email_template.html"
+    me = "Bruna"
+    mail = prepare_message(tf, me)
+
+    r1 = {'name':"Bruna", 'email':"aaaaa@gmail.com" }
+    r2 = {'name':"Brenda", 'email':"aaaaa@gmail.com" }
+    r3 = {'name':"Matheus", 'email':"aaaaaa@gmail.com" }
+    maillist = [r1, r2, r3]
+
+    print("send one")
+    send_to_one(r1, mail)
+    print("send list")
+    send_to_list(maillist, mail)
+
+
+main()
+
+
+
+
+##### unused
+def send_my_email(msg):
     soup = get_template(template_file)
     receiver = {'name':receiver_name, 'email':receiver_email }
 
@@ -73,8 +116,7 @@ def send_my_email(template_file, sender_name, receiver_name, receiver_email):
     mail = change_receiver(mail, receiver)
 
     send(mail)
+# send_my_email("email_template.html", "Bruna", "Matheus", "aaaaa@gmail.com")
 
 
-
-send_my_email("email_template.html", "Bruna", "Matheus", "brunanayaramlima@gmail.com")
 
