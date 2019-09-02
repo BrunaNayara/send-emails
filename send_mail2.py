bdf5e81492b72c6ec9806a966e514d2340d5cf39
Bruna Moreira <brunanayaramlima@gmail.com>
2019-09-01 23:52:32 -0300

    Add send same and custom mail functions

--- send_mail2.py
+++ send_mail2.py
@@ -10,11 +10,11 @@ from bs4 import BeautifulSoup
 
 ##### Beautiful Soup methods
 # Get email template
-def get_template(template):
-    with open(template) as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+def get_template(template_file):
+    with open(template_file) as fp:
+        template = BeautifulSoup(fp, 'html.parser')
 
-    return soup
+    return template
 
 # Greetings human
 def custom_greeting(name, template):
@@ -33,10 +33,20 @@ def change_sender_name(sender, template):
 
 ##### Email and mime methods
 # create mail message
-def create_mail(subject, body):
+def create_mail(subject, body, sender):
     msg = MIMEText(body, 'html')
     msg['Subject'] = subject
-    msg['From'] = "brunanayaramlima@gmail.com"
+    msg['From'] = sender
+
+    return msg
+
+# create custom mail (change greetings)
+def create_custom_mail(subject, body, receiver, sender):
+    soup = custom_greeting(receiver['name'], body)
+
+    subject = receiver['name'] + ", " + subject
+
+    msg = create_mail(subject, soup, sender)
 
     return msg
 
@@ -62,7 +72,6 @@ def send(msg):
     server.login(me, p)
     server.send_message(msg)
     server.quit()
-
 
 ##### Send functions
 def send_to_list(mail_list, msg):
@@ -97,7 +106,34 @@ def main():
     send_to_list(maillist, mail)
 
 
-main()
+def send_custom_mails():
+    tf = "email_template.html"
+    me = "Bruna"
+    subject = "Assunto personalizado"
+
+    r1 = {'name':"Bruna", 'email':"aaaaaa@gmail.com" }
+    r2 = {'name':"Brenda", 'email':"aaaaa@gmail.com" }
+    r3 = {'name':"Matheus", 'email':"aaaaaaa@gmail.com" }
+    maillist = [r1, r2, r3]
+
+    body = get_template(tf)
+
+    for receiver in maillist:
+        mail = create_custom_mail(subject, body, receiver, me)
+        send_to_one(receiver, mail)
+
+
+
+def main():
+    # TODO change main to send custom mail to the list
+
+    # send_same_email()
+    send_custom_mails()
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
